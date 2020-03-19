1fa850273b23a2694c9de5c75a38f02b70abd847
skbisht <62379959+skbisht@users.noreply.github.com>
2020-03-19 16:27:32 +0530

    Create testscript.js

---
+++ testscript.js
@@ -0,0 +1,6 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="text/javascript">
+  export function apiCall(){
+    alert("calling API");
+  }
+</script>
